514f5943e481ff0d4c1ded28eb40242350485ff5
Basanta <basantaparajuli.11@gmail.com>
2025-07-11 16:34:52 +0545

    writing a sum func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main(){
-    println!("Basantalovesjs!") // this is a macro call. if ! is used before function call then its macro!
+    println!("Basantalovesjs!"); // this is a macro call. if ! is used before function call then its macro! and macro means this code will get broken down into more after soemtime!
+
+    let ans = sum(1,2);
+    println!("{}", ans)
+
+}
+
+fn sum (a: u32, b: u32) -> u32{
+    return a + b;
 }
